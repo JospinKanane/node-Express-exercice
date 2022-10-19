6e8ba77edefaf44417c9b642bb2948fab697dcf6
Jospin Kanane <jospinkanane@yahoo.com>
2022-10-19 16:25:57 +0200

    exercice expressjs

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + `/pages/index.html`);
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname+`/pages/contact.html`);
+});
+
+app.get('/visit', (req, res) => {
+    res.sendFile(__dirname+`/pages/visit.html`);
+});
+
+
+app.listen(3000, ()=>{
+    console.log('listening on port 5522');
+});
